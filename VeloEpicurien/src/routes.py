80525c02cd85ce6dcd511b8e1e15eaf5b100acc4
Petros Fytilis <petros.fytilis.1@ulaval.ca>
2021-09-26 01:23:14 -0400

    Connecte mongodb à flask

--- VeloEpicurien/src/routes.py
+++ VeloEpicurien/src/routes.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://mongodb:27017')
+db = client.test
 
 views = Blueprint('views', __name__)
 
@@ -8,3 +12,12 @@ villeChoisie = {'villeChoisie': "Montreal"}
 @views.route("/heartbeat")
 def get_json():
     return jsonify(villeChoisie)
+
+
+@views.route('/cities', methods=['GET', 'POST'])
+def cities():
+    if request.method == 'POST':
+        db.cities.insert_one(request.json)
+        return '', 201
+    elif request.method == 'GET':
+        return jsonify(list(db.cities.find(projection={"_id": 0}))), 200
