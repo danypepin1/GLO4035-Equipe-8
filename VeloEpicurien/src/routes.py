acce8429ef45a5c3d7e7931c2a1692a136ed73a4
danypepin1 <dany.pepin.1@ulaval.ca>
2021-10-27 19:30:35 -0400

    partie restaurant de transformed_data

--- VeloEpicurien/src/routes.py
+++ VeloEpicurien/src/routes.py
@@ -45,3 +45,21 @@ def segments():
     elif request.method == 'DELETE':
         db.segments.drop()
         return '', 200
+
+
+@views.route('/transformed_data', methods=['GET', 'DELETE'])
+def transformed_date():
+    if request.method == 'GET':
+        return jsonify(
+            {
+                'Restaurants': list(db.restaurants.aggregate([{'$unwind': '$categories'},
+                                                              {'$group': {'_id': '$categories.title',
+                                                                          'count': {'$sum': 1}}},
+                                                              {'$project': {'type': '$_id', 'count': 1, '_id': 0}}, {
+                                                                  '$replaceRoot': {'newRoot': {'$arrayToObject': [
+                                                                      [{'k': '$type', 'v': '$count'}]]}}}]))
+            }
+        ), 200
+    elif request.method == 'DELETE':
+        db.transformed_date.drop()
+        return '', 200
