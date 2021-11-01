0c9c13da44ee22f298754f473d413b056d0b269e
Petros Fytilis <petros.fytilis.1@ulaval.ca>
2021-11-01 18:25:55 -0400

    Transformation des données

--- VeloEpicurien/scripts/extract_restaurants.py
+++ VeloEpicurien/scripts/extract_restaurants.py
@@ -8,9 +8,32 @@ from pymongo import MongoClient
 def extract_restaurants(db, path):
     print('Extracting restaurants...')
     with open(path) as f:
-        restaurants = json.load(f)
-        db.restaurants.insert_many(restaurants)
-        print(f'Extracted {len(restaurants)} restaurants.')
+        old_restaurants = db.restaurants.find()
+        new_restaurants = [
+            new_restaurant for new_restaurant in json.load(f)
+            if not any(old_restaurant for old_restaurant in old_restaurants if old_restaurant.id == new_restaurant.id)
+        ]
+        db.restaurants.insert_many(new_restaurants)
+        print(f'Extracted {len(new_restaurants)} restaurants.')
+
+
+def generate_restaurants_view(db):
+    print('Generating restaurants view...')
+    restaurants = db.restaurants.aggregate({
+        '$project': {
+            '_id': False,
+            'id': True,
+            'name': True,
+            'categories': True,
+            'geometry': {
+                'type': 'Point',
+                'coordinates': ['$coordinates.longitude', '$coordinates.latitude']
+            }
+        }}
+    )
+    db.restaurants_view.drop()
+    db.restaurants_view.insert_many(restaurants)
+    print('Finished generating restaurants view.')
 
 
 def generate_types_view(db):
@@ -31,6 +54,7 @@ def main(path):
     client = MongoClient(os.environ.get('MONGO_URI'))
     db = client.test
     extract_restaurants(db, path)
+    generate_restaurants_view(db)
     generate_types_view(db)
 
 
--- VeloEpicurien/scripts/extract_segments.py
+++ VeloEpicurien/scripts/extract_segments.py
@@ -13,10 +13,23 @@ def extract_segments(db, path):
         print(f'Extracted {len(segments)} segments.')
 
 
+def generate_segments_view(db):
+    print('Generating segments view...')
+    segments = db.segments.find(project={
+        '_id': False,
+        'properties.LONGUEUR': True,
+        'geometry': True
+    })
+    db.segments_view.drop()
+    db.segments_view.insert_many(segments)
+    print('Finished generating segments view.')
+
+
 def main(path):
     client = MongoClient(os.environ.get('MONGO_URI'))
     db = client.test
     extract_segments(db, path)
+    generate_segments_view(db)
 
 
 if __name__ == '__main__':
