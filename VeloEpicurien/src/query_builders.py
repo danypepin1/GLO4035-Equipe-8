95b7e1d62fb9ab5b8d6dde5e5ebd0850e48b30d6
Petros Fytilis <petros.fytilis.1@ulaval.ca>
2021-12-05 00:06:43 -0500

    Implémente la route /parcours, reste à gérer les edge cases

---
+++ VeloEpicurien/src/query_builders.py
@@ -0,0 +1,69 @@
+DEFAULT_NB_STOPS = 10
+
+
+def build_starting_point_query(length, types):
+    query = 'MATCH p=((starting_point:Junction)-[c1:connects_to]->(r1:Restaurant)'
+    for i in range(2, DEFAULT_NB_STOPS + 1):
+        query += f'-[c{i}:shortest_path_to]->(r{i}:Restaurant)'
+    query += ')\n'
+
+    query += 'WHERE c1.length'
+    for i in range(2, DEFAULT_NB_STOPS + 1):
+        query += f' + c{i}.total_length'
+    query += f' < {length * 1.1}\n'
+
+    for i in range(1, DEFAULT_NB_STOPS):
+        for j in range(i + 1, DEFAULT_NB_STOPS + 1):
+            query += f'AND r{i}.name <> r{j}.name '
+        query += '\n'
+
+    if len(types) > 0:
+        query += f'AND ANY(r IN [r1'
+        for i in range(2, DEFAULT_NB_STOPS + 1):
+            query += f', r{i}'
+        query += f'] WHERE ANY(type in {str(types)} WHERE type in r.types))\n'
+
+    query += 'RETURN starting_point, r1'
+    for i in range(2, DEFAULT_NB_STOPS + 1):
+        query += f', r{i}'
+    query += ' LIMIT 1'
+
+    print(f'[QUERY]\n{query}')
+    return query
+
+
+def build_itinerary_query(nb_stops, restaurant_ids, length):
+    query = 'MATCH\n'
+    query += 'p1=shortestPath((r1:Restaurant)-[:connects_to*..25]->(r2:Restaurant))'
+    for i in range(2, nb_stops):
+        query += f',\np{i}=shortestPath((r{i})-[:connects_to*..25]->(r{i+1}:Restaurant))'
+    query += f',\np{nb_stops}=(r{nb_stops})-[:connects_to*]->()\n'
+
+    query += f'WHERE ID(r1) = {restaurant_ids[0]}'
+    for i in range(2, nb_stops + 1):
+        query += f'\nAND ID(r{i}) = {restaurant_ids[i - 1]}'
+
+    query += '\nWITH'
+    for i in range(1, nb_stops + 1):
+        query += f'\nreduce(acc=0, r IN relationships(p{i}) | acc + r.length) AS l{i},'
+
+    query += '\np1'
+    for i in range(2, nb_stops + 1):
+        query += f', p{i}'
+
+    query += f'\nWHERE {length * 0.9} < l1'
+    for i in range(2, nb_stops + 1):
+        query += f' + l{i}'
+    query += f' < {length * 1.1}\n'
+
+    query += 'RETURN p1'
+    for i in range(2, nb_stops + 1):
+        query += f', p{i}'
+    query += ', l1'
+    for i in range(2, nb_stops + 1):
+        query += f', l{i}'
+
+    query += '\nLIMIT 1'
+
+    print(f'[QUERY]\n{query}')
+    return query
--- VeloEpicurien/src/routes.py
+++ VeloEpicurien/src/routes.py
@@ -2,8 +2,9 @@ import os
 from flask import Blueprint, jsonify, request
 from py2neo import Graph
 from pymongo import MongoClient
+from src.query_builders import build_starting_point_query, build_itinerary_query
 
-DEFAULT_NB_STOPS = 10
+JUNCTION = 'Junction'
 CHOSEN_CITY = {'villeChoisie': "Montreal"}
 
 client = MongoClient(os.environ.get('MONGO_URI'))
@@ -35,8 +36,7 @@ def transformed_data():
     return jsonify({
         'restaurants': _fetch_restaurant_types(),
         'longueurCyclable': _fetch_segments_length()
-    }
-    ), 200
+    }), 200
 
 
 @views.route('/type')
@@ -47,17 +47,125 @@ def get_restaurant_types():
 
 @views.route('/starting_point')
 def get_starting_point():
-    query = _build_starting_point_query(
-        request.json['length'],
-        request.json['type']
-    )
-    starting_point = graph.evaluate(query)
+    error = _validate_starting_point_params(request.json)
+    if error:
+        return error, 400
+    length = request.json['length']
+    types = request.json['type']
+    query = build_starting_point_query(length, types)
+    cursor = graph.run(query)
+    cursor.forward()
+    starting_point = cursor.current['starting_point']
     return jsonify({
         'startingPoint': {
             'type': 'Point',
             'coordinates': [starting_point['long'], starting_point['lat']]
         }
     }), 200
+
+
+def _validate_starting_point_params(json):
+    error = ''
+    if 'length' not in json:
+        error += 'Must include parameters "type"\n'
+    elif not (isinstance(json['length'], int) or isinstance(json['length'], float)):
+        error += 'Parameter "length" must be a number\n'
+    if 'type' not in json:
+        error += 'Must include parameters "length"\n'
+    else:
+        if not (isinstance(json['type'], list)):
+            error += 'Parameter "type" must be a list\n'
+        if not all(isinstance(t, str) for t in json['type']):
+            error += 'Parameter "type" must be a list of strings\n'
+    return error
+
+
+@views.route('/parcours')
+def get_itinerary():
+    error = _validate_itinerary_params(request.json)
+    if error:
+        return error, 400
+    length = request.json['length']
+    types = request.json['type']
+    number_of_stops = request.json['numberOfStops']
+    query = build_starting_point_query(length, types)
+    cursor = graph.run(query)
+    cursor.forward()
+    query = build_itinerary_query(
+        number_of_stops,
+        [cursor.current[f'r{i}'].identity for i in range(1, number_of_stops + 1)],
+        length
+    )
+    cursor = graph.run(query)
+    cursor.forward()
+    return jsonify(_build_itinerary_geojson(cursor.current, number_of_stops, types)), 200
+
+
+def _build_itinerary_geojson(result, number_of_stops, types):
+    features = []
+    for i in range(1, number_of_stops + 1):
+        path = result[f'p{i}']
+        restaurant = path.start_node
+        features.append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [
+                    restaurant['long'],
+                    restaurant['lat']
+                ]
+            },
+            'properties': {
+                'name': restaurant['name'],
+                'type': _get_correct_type(restaurant['types'], types)
+            }
+        })
+        segment = []
+        for relationship in path.relationships:
+            end_node = relationship.end_node
+            if JUNCTION in end_node.labels:
+                segment.append(
+                    [end_node['long'], end_node['lat']]
+                )
+        if len(segment) > 1:
+            features.append({
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'MultiLineString',
+                    'coordinates': [segment]
+                },
+                'properties': {
+                    'length': result[f'l{i}']
+                }
+            })
+    return {'type': 'FeatureCollection', 'features': features}
+
+
+def _validate_itinerary_params(json):
+    error = ''
+    if not (isinstance(json['startingPoint'], dict)):
+        error += 'Parameter "startingPoint" must be a dictionary\n'
+    if 'length' not in json:
+        error += 'Must include parameters "type"\n'
+    elif not (isinstance(json['length'], int) or isinstance(json['length'], float)):
+        error += 'Parameter "length" must be a number\n'
+    if not (isinstance(json['numberOfStops'], int)):
+        error += 'Parameter "numberOfStops" must be an integer\n'
+    if 'type' not in json:
+        error += 'Must include parameters "length"\n'
+    else:
+        if not (isinstance(json['type'], list)):
+            error += 'Parameter "type" must be a list\n'
+        if not all(isinstance(t, str) for t in json['type']):
+            error += 'Parameter "type" must be a list of strings\n'
+    return error
+
+
+def _get_correct_type(restaurant_types, wanted_types):
+    for restaurant_type in restaurant_types:
+        if restaurant_type in wanted_types:
+            return restaurant_type
+    return restaurant_types[0]
 
 
 def _fetch_restaurant_types():
@@ -76,31 +184,3 @@ def _fetch_segments_length():
         return 0. + cursor[0]['total']
     else:
         return 0.
-
-
-def _build_starting_point_query(length, types):
-    query = 'MATCH p=((starting_point:Junction)-[c1:connects_to]->(r1:Restaurant)'
-    for i in range(2, DEFAULT_NB_STOPS + 1):
-        query += f'-[c{i}:shortest_path_to]->(r{i}:Restaurant)'
-    query += ')\n'
-
-    query += 'WHERE c1.length'
-    for i in range(2, DEFAULT_NB_STOPS + 1):
-        query += f' + c{i}.total_length'
-    query += f' < {length * 1.1}\n'
-
-    for i in range(1, DEFAULT_NB_STOPS):
-        for j in range(i + 1, DEFAULT_NB_STOPS + 1):
-            query += f'AND r{i}.name <> r{j}.name '
-        query += '\n'
-
-    if len(types) > 0:
-        query += f'AND ANY(r IN [r1'
-        for i in range(2, DEFAULT_NB_STOPS + 1):
-            query += f', r{i}'
-        query += f'] WHERE ANY(type in {str(types)} WHERE type in r.types))\n'
-
-    query += 'RETURN starting_point LIMIT 1'
-
-    print(f'[QUERY]\n{query}')
-    return query
